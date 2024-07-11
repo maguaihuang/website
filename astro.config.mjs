fa29ae15aaa08d19302e6d9dde4520fb197932ea
YuHan Huang 黃郁涵 <yuhanhuang@hitrust.com.tw>
2024-07-11 17:56:34 +0800

    建立astro框架網站

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    viewTransitions: true,
+  },
+  integrations: [tailwind(), compress()],
+  site: 'https://maguaihuang.github.io/'
+  // base: 'my-repo',
+});
